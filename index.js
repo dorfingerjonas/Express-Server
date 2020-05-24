bc46445859cae55c8df36999654fb7bc59906b7f
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-05-24 17:37:52 +0200

    Update and rename app.js to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(3000, () => {
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.post('/chat', (req, res) => {
     if (req.body.name && req.body.message) {
-        let line = `${req.body.name};${req.body.message}\n`;
+        const line = `${req.body.name};${req.body.message}\n`;
+
         fs.appendFile('public/data/chat.csv', line, 'utf8', (err) => {
             if (err) throw err;
             console.log('@file chat.csv: new data added.');
@@ -32,7 +33,7 @@ app.get('/chat', (req, res) => {
     const chat = fs.readFileSync('public/data/chat.csv', 'utf8');
     let chatHtml = '';
 
-    for (let line of chat.split('\n')) {
+    for (const line of chat.split('\n')) {
         if (line) {
             line = line.split(';');
 
@@ -48,17 +49,17 @@ app.get('/chat/:id', (req, res) => {
        if (err) {
            res.status(404).send('Sorry no messages stored.');
        } else {
-           let lines = data.split('\n');
+           const lines = data.split('\n');
 
            if (req.params.id > 0 && req.params.id <= lines.length) {
-               let line = lines[req.params.id-1];
-               let elements = line.split(';');
+               const line = lines[req.params.id-1];
+               const elements = line.split(';');
 
-               if(elements[1] !== undefined) {
-                   let obj = {name: elements[0], age: elements[1]};
+               if (elements[1] !== undefined) {
+                   const obj = {name: elements[0], age: elements[1]};
                    res.send(JSON.stringify(obj));
                } else {
-                   let obj = {error: "empty message" };
+                   const obj = {error: 'empty message' };
                    res.send(JSON.stringify(obj));
                }
            } else {
